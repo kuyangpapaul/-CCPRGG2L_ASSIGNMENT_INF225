3d441b8524c6aacd4e3703565c69fa126a03cb8a
Paul Vincent C. Mercado <119905612+kuyangpapaul@users.noreply.github.com>
2023-01-27 10:59:57 +0800

    Create App.java

---
+++ Exercise_4/App.java
@@ -0,0 +1,21 @@
+import javax.swing.ProgressMonitorInputStream;
+
+public class App {
+    public static void main(String[] args) {
+
+       Person friend = new Person("Abi", 18);
+
+       Myself me = new Myself("Paul", 20);
+
+       me.addFriend(friend);
+       
+       Pet dog = new Pet("Bandar", friend);
+
+       dog.showOwnerName();
+
+       Car Honda = new Car("Honda Civic type R", me , 169 );
+
+       Honda.showCarName();
+
+    }   
+}
